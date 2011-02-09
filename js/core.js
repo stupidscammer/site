5dd66ae042a053b59f53b8386c4a3dd466097842
lucalysoft <lucaly17@gmail.com>
2011-02-09 00:16:14 -0800

    New approach for an output area

--- js/core.js
+++ js/core.js
@@ -11,4 +11,22 @@ $(document).ready(function () {
 
         passed = JSHINT(code, options);
     });
+
+    $("div.sidebar nav ul li a").live("click", function (ev) {
+        ev.preventDefault();
+
+        var parent = $(this).parent('li'),
+            target = parent.attr('data-target');
+
+        $(this).closest('ul').children('li:not(:has(a))').each(function () {
+            var a = document.createElement('A');
+            a.href = '#';
+            a.innerHTML = $(this).html();
+            $(this).html(a);
+        });
+        parent.html($(this).html());
+
+        $('div.sidebar div.wrapper').addClass('hidden');
+        $('div.sidebar div.wrapper.' + target).removeClass('hidden');
+    });
 });

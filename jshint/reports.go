05bc50b3425dc3fbad92a30cf5e6d575fe24633d
lucalysoft <lucaly17@gmail.com>
2012-05-28 00:32:17 -0700

    Update report.go to use the new SDK

--- jshint/reports.go
+++ jshint/reports.go
@@ -3,23 +3,23 @@ package reports
 import (
 	"appengine"
 	"appengine/datastore"
-	"http"
-	"time"
-	"template"
+	"html/template"
+	"net/http"
 	"strconv"
+	"time"
 )
 
 type Report struct {
 	Code []byte
 	Opts []byte
-	Date datastore.Time
+	Date time.Time
 }
 
 var templates = make(map[string]*template.Template)
 
 func init() {
 	for _, name := range []string{"500", "404", "report"} {
-		tmpl := template.Must(template.New(name).ParseFile("jshint/templates/" + name + ".html"));
+		tmpl := template.Must(template.New(name).ParseFiles("jshint/templates/" + name + ".html"))
 		templates[name] = tmpl
 	}
 
@@ -29,9 +29,9 @@ func init() {
 }
 
 func renderTemplate(w http.ResponseWriter, name string, report *Report) {
-	err := templates[name].Execute(w, report)
+	err := templates[name].ExecuteTemplate(w, name+".html", report)
 	if err != nil {
-		http.Error(w, err.String(), 500)
+		http.Error(w, err.Error(), 500)
 	}
 }
 
@@ -41,17 +41,17 @@ func save(w http.ResponseWriter, r *http.Request) {
 	report := Report{
 		Code: []byte(r.FormValue("code")),
 		Opts: []byte(r.FormValue("data")),
-		Date: datastore.SecondsToTime(time.Seconds()), // Now
+		Date: time.Now(),
 	}
 
 	key, err := datastore.Put(c, datastore.NewIncompleteKey(c, "report", nil), &report)
 	if err != nil {
-		c.Criticalf(err.String())
+		c.Criticalf(err.Error())
 		renderTemplate(w, "500", nil)
 		return
 	}
 
-	w.Header().Set("Location", "/reports/"+strconv.Itoa64(key.IntID()))
+	w.Header().Set("Location", "/reports/"+strconv.FormatInt(key.IntID(), 10))
 	w.WriteHeader(http.StatusFound)
 }
 
@@ -61,10 +61,10 @@ func show(w http.ResponseWriter, r *http.Request) {
 	var report Report
 	c := appengine.NewContext(r)
 
-	intID, _ := strconv.Atoi64(r.URL.Path[lenPath:])
+	intID, _ := strconv.ParseInt(r.URL.Path[lenPath:], 10, 64)
 	key := datastore.NewKey(c, "report", "", intID, nil)
 	if err := datastore.Get(c, key, &report); err != nil {
-		c.Criticalf(err.String())
+		c.Criticalf(err.Error())
 		renderTemplate(w, "404", nil)
 		return
 	}
